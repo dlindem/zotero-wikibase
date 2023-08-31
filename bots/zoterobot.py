f2a0326ca2caa06c9188a8c26340aa95bc08279e
David Lindemann <34068491+dlindem@users.noreply.github.com>
2023-09-01 00:10:06 +0200

    Update zoterobot.py

--- bots/zoterobot.py
+++ bots/zoterobot.py
@@ -56,4 +56,97 @@ def getexport():
 	exportitems = pyzot.items(tag=config.zotero_export_tag)
 	return exportitems
 
+def getchildren(zotitemid):
+	children = pyzot.children(zotitemid)
+	return children
+
+def patch_item(qid=None, zotitem=None, children = []):
+	# communicate with Zotero, write Wikibase entity URI to "extra" and attach URI as link attachment
+
+		zotitem['data']['extra'] = config.entity_ns + qid + "\n" + zotitem['data']['extra']
+		pyzot.update_item(zotitem)
+		# attempts = 0
+		# while attempts < 5:
+		# 	attempts += 1
+		# 	r = requests.patch(zotapid,
+		# 					   headers={"Zotero-API-key": config_private.zotero_api_key},
+		# 					   json={"extra": config.entity_ns + qid + "\n" + zotitem['data']['extra'],
+		# 							 "version": zotitem['version']})
+		#
+		# 	if "204" in str(r):
+		# 		print('Successfully patched zotero item ' + zotitemid + ': ' + bibItemQid)
+		# 		# with open(config_private.datafolder+'zoteroapi/lwbqid2zotero.csv', 'a', encoding="utf-8") as logfile:
+		# 		# 	logfile.write(qid+','+zotitemid+'\n')
+		# 		break
+		# 	print(
+		# 		'Zotero API PATCH request failed (' + zotitemid + ': ' + bibItemQid + '), will repeat. Response was ' + str(
+		# 			r) + str(r.content))
+		# 	time.sleep(2)
+		#
+		# if attempts > 4:
+		# 	print('Abort after 5 failed attempts.')
+		# 	sys.exit()
+
+		# check for presence of link attachment
+		linkattachment = False
+		for child in children:
+			if child['data']['url'].startswith(config.entity_ns):
+				if child['data']['url'].endswith(qid):
+					print('Correct link attachment already present.')
+				else:
+					print('Fatal error: Zotero item was linked before to some other Q-id')
+			else:
+
+
+		# attempts = 0
+		# while attempts < 5:
+		# 	attempts += 1
+		# 	r = requests.get(zotapid + "/children")
+		# 	if "200" in str(r):
+		# 		zotitem = r.json()
+		# 		print(zotitemid + ': got zotitem attachment data')
+		# 		break
+		# 	if "400" or "404" in str(r):
+		# 		print(
+		# 			'*** Fatal error: Item ' + zotitemid + ' got ' + str(r) + ', does not exist on Zotero. Will skip.')
+		# 		time.sleep(5)
+		# 		break
+		# 	print('Zotero API GET request failed (' + zotitemid + '), will repeat. Response was ' + str(r))
+		# 	time.sleep(2)
+		#
+		# if attempts < 5:
+		# 	att_presence = None
+		# 	for attachmnt in r.json():
+		# 		if 'title' in attachmnt['data']:
+		# 			if attachmnt['data']['title'] == "LexBib Linked Data":
+		# 				att_presence = True
+		# 				break
+		#
+		# if not att_presence:
+			# attach link to wikibase
+				attachment = [
+					{
+						"itemType": "attachment",
+						"parentItem": zotitem['data']['key'],
+						"linkMode": "linked_url",
+						"title": config.wikibase_name,
+						"accessDate": time.strftime("%Y-%m-%dT%H:%M:%SZ"),
+						"url": config.entity_ns + qid,
+						"note": '<p>See this item as linked data at <a href="'+config.wikibase_url+'/wiki/Item:' + qid + '">'+config.entity_ns+ qid + '</a>',
+						"tags": [],
+						"collections": [],
+						"relations": {},
+						"contentType": "",
+						"charset": ""
+					}
+				]
+
+				r = requests.post('https://api.zotero.org/groups/'+str(config.zotero_group_id)+'/items',
+								  headers={"Zotero-API-key": config_private.zotero_api_key,
+										   "Content-Type": "application/json"}, json=attachment)
+
+				if "200" in str(r):
+					print(f"Link attachment successfully attached to Zotero item {zotitem['data']['key']}.")
+
+
 
